92f275a93798747a0c754178ae078b6ed315958d
Parham Shagerdmootab <psm1379@gmail.com>
2022-10-12 17:34:24 -0700

    Changed PhotosStorage to Azure Durable Function

---
+++ PhotosAnalyzer/PhotosAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using PhotosAnalyzer.AnalyzerService.Abstarctions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotosAnalyzer
+{
+    public class PhotosAnalyzer
+    {
+        private readonly IAnalyzerService analyzerService;
+
+        public PhotosAnalyzer(IAnalyzerService analyzerService)
+        {
+            this.analyzerService = analyzerService;
+        }
+
+        [FunctionName("PhotosAnalyzer")]
+        public async Task<dynamic> Run([ActivityTrigger] List<byte> image)
+        {
+            return await analyzerService.AnalyzeAsync(image.ToArray());
+        }
+    }
+}
